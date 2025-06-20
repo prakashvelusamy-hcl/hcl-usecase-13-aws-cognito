1d87aa7160cf97fcd1393b083c003e3f24dd363e
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-20 15:18:22 +0530

    fix

--- modules/terraform-aws-apigateway/output.tf
+++ modules/terraform-aws-apigateway/output.tf
@@ -1,6 +1,3 @@
-output "aws_apigatewayv2_arn" {
-    value = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
-}
 # output "api_url" {
 #   value = aws_apigatewayv2_api.http_api.api_endpoint
 # }
