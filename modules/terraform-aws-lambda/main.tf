038b299d5e0c516e4e89f3305c09cfec86ecb8ba
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-20 17:52:02 +0530

    fix

--- modules/terraform-aws-lambda/main.tf
+++ modules/terraform-aws-lambda/main.tf
@@ -81,7 +81,7 @@ resource "aws_lambda_function" "lambda" {
 resource "aws_lambda_permission" "apigw_invoke" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.docker_lambda.function_name
+  function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn = "${var.api_gateway_arn}/*/GET/test"
 }
